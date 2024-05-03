77623cba56ebffa8721be2a6b090faa1b80e6a5d
Dani Ona <dani@kodaris.com>
2024-05-03 22:40:56 +0800

    update readme; comments

--- CTestAppFrontend/src/app/Services/items-service.ts
+++ CTestAppFrontend/src/app/Services/items-service.ts
@@ -12,22 +12,47 @@ export class ItemsService {
 
   constructor(private _http: HttpClient) {}
 
+  /**
+   * Gets all the Items.
+   * @returns Item[]
+   */
   public getAllItems(): Observable<Item[]> {
     return this._http.get<Item[]>(`${this.BASE_URL}/api/Items`);
   }
 
+  /**
+   * Adds an Item.
+   * @param item the item to add
+   * @returns the post response
+   */
   public addItem(item: Item) {
     return this._http.post(`${this.BASE_URL}/api/Items`, item, { headers: this.HEADERS });
   }
 
+  /**
+   * Deletes and Item given its ID.
+   * @param itemId ID of the Item
+   * @returns the delete response
+   */
   public deleteItem(itemId: number) {
     return this._http.delete(`${this.BASE_URL}/api/Items/${itemId}`);
   }
 
+  /**
+   * Edits an Item.
+   * @param itemId ID of the Item
+   * @param item the edited item
+   * @returns the put response
+   */
   public editItem(itemId: number, item: Item) {
     return this._http.put(`${this.BASE_URL}/api/Items/${itemId}`, item, { headers: this.HEADERS });
   }
 
+  /**
+   * Gets an Item given its ID.
+   * @param itemId ID of the item
+   * @returns the requested item
+   */
   public getItem(itemId: number): Observable<Item> {
     return this._http.get(`${this.BASE_URL}/api/Items/${itemId}`);
   }
---
+++ README.md
@@ -0,0 +1,23 @@
+# Test App
+
+This is a test app using Angular 17 as the Frontend and .NET Core 8 for the backend.
+
+## Building the Frontend
+```bash
+$> cd CTestAppFrontned
+$> npm install
+$> npm start
+```
+
+This will run under `localhost:4200`.
+
+## Building the Backend
+```
+$> cd CTestAppBackend
+$> dotnet run
+```
+
+This will run under `localhost:5178` and will launch the Swagger page.
+
+## Note
+I wasn't able to initialize the git repository for the whole project before I started coding, that's why this repository has one big commit.
